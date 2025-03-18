52940b996fa10bc64e31bdeec64517be807419eb
NavyBlue06 <ozpiddkk@students.codeinstitute.net>
2025-03-18 12:29:39 +0100

    create booking model, Apply migrations

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Reservation (models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.IntegerField()
+    message = models.TextField()
+    
+    def __str__(self):
+        return f"Reservation for {self.name} on {self.date} at {self.time}"
