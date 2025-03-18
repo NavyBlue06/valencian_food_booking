589db383306f0535ccb394ea69b6e080877130d0
NavyBlue06 <ozpiddkk@students.codeinstitute.net>
2025-03-18 12:45:35 +0100

    create booking view, form submissions and save bookings in the database

--- reservations/views.py
+++ reservations/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .forms import ReservationForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+def book_table(request):
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ReservationForm()
+    return render(request, 'book_table.html', {'form': form})
