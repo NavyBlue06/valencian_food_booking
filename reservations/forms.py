37048b00e4b42ee05174b5f276a45b5c352c386e
NavyBlue06 <ozpiddkk@students.codeinstitute.net>
2025-03-20 14:42:20 +0100

    modify and correct code

--- reservations/forms.py
+++ reservations/forms.py
@@ -1,10 +1,19 @@
 from django import forms
-from .models import Reservation
+from reservations.models import Reservation
+
 
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
-        fields = ['name', 'phone', 'email', 'date', 'time', 'guests', 'message']'
+        fields = [
+            'name', 
+            'phone',
+            'email', 
+            'date', 
+            'time', 
+            'guests', 
+            'message',
+        ]
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
             'time': forms.TimeInput(attrs={'type': 'time'}),
