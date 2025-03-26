81a47f2041ae5e0163da86dc45428023027d438a
NavyBlue06 <ozpiddkk@students.codeinstitute.net>
2025-03-26 17:53:52 +0100

    Create edit_booking view

--- reservations/views.py
+++ reservations/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages  # import messages
 from reservations.forms import ReservationForm
 from django.contrib.auth.decorators import login_required
-
+from .models import Booking
+from .forms import BookingForm
+from django.contrib.auth.models import login_required
 # Create your views here.
 
 
@@ -24,3 +26,16 @@ def book_table(request):
 def my_bookings(request):
     bookings= Booking.objects.filter(user=request.user)
     return render(request, 'my_bookings.html', {'bookings': bookings})
+
+@login_required
+def edit_booking(request, booking_id):
+     booking = get_object_or_404(Booking, id=booking_id, user=request.user)
+
+     if request.method == 'POST':
+         form = BookingForm(request.POST, instance=booking)
+            if form.is_valid():
+                form.save()
+                return redirect('my_bookings')
+     else:
+         form =bookingForm(instance=booking)
+         return render ( request, 'reservations/templates/edit_booking.html', {'form':form})
